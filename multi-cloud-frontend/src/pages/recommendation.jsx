459db6c584bf5d5b71ea608f6f538c3d1c25885b
arshiumair <arshadatf16@gmail.com>
2025-08-11 20:19:29 +0500

    renamed form

--- multi-cloud-frontend/src/pages/recommendation.jsx
+++ multi-cloud-frontend/src/pages/recommendation.jsx
@@ -14,7 +14,7 @@ const Recommendation = ({ result }) => {
   return (
     <div className="min-h-screen bg-indigo-950 flex flex-col items-center justify-center">
       <div className="w-full max-w-4xl p-8 bg-gray-900 rounded-lg shadow-lg text-white mb-8">
-        <h2 className="text-2xl font-bold mb-6 text-indigo-400">Gemini Recommendation</h2>
+        <h2 className="text-2xl font-bold mb-6 text-indigo-400">Copilot Recommendation</h2>
         <div className="bg-gray-800 text-indigo-100 p-8 rounded-lg shadow-lg">
           <MarkdownRenderer content={result} />
         </div>
