0639e0e4fe3096eb01fa34717cd90a60e2ab1102
Arshad <154744306+arshiumair@users.noreply.github.com>
2025-08-11 22:01:39 +0500

    Update setup_backend.sh

--- backend/setup_backend.sh
+++ backend/setup_backend.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Update system
-sudo dnf update -y
+sudo yum update -y
 
 # Install Python 3 and pip
-sudo dnf install -y python3 python3-pip python3-venv
+sudo yum install -y python3 python3-pip python3-venv
 
 # Create and activate virtual environment
 python3 -m venv venv
